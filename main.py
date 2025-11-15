bde9e9b9e860ff8030559e4ca54d159e7b78574d
tzsiga <zsiga0tamas@gmail.com>
2025-11-15 12:26:56 +0100

    add refresh rate to game loop

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,7 +18,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
