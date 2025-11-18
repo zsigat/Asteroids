fc87b61d880058e9fc053ab2c39dc9f3088e262b
tzsiga <zsiga0tamas@gmail.com>
2025-11-18 19:37:58 +0100

    add collision check for shot vs asteroid

--- main.py
+++ main.py
@@ -41,6 +41,14 @@ def main():
         screen.fill("black")
         updatable.update(dt)
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
+        for asteroid in asteroids:
+            
             if asteroid.collides(player):
                 log_event("player_hit")
                 print("Game over!")
